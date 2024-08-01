87b64fc4f220c94bb5a43d748cda2b76ca0d9af4
e-sparrow <51369840+e-sparrow@users.noreply.github.com>
2024-08-01 06:01:37 +0300

    init

---
+++ Assets/Scripts/Core/Player/PlayerModel.cs
@@ -0,0 +1,31 @@
+using System;
+using Game.Core.Abstractions;
+
+namespace Game.Core.Player
+{
+    public sealed class PlayerModel
+        : IDamageable
+    {
+        public PlayerModel(int health)
+        {
+            Health = health;
+        }
+        
+        public event Action OnDeath = () => { };
+        public event Action<int> OnHealthChanged = _ => { };
+        
+        public int Health
+        {
+            get; 
+            private set;
+        }
+
+        public bool Damage(int amount)
+        {
+            Health -= amount;
+            OnHealthChanged.Invoke(Health);
+            
+            return Health <= 0;
+        }
+    }
+}
